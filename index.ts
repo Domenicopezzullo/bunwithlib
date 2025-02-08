879621cc8d22b56bc6389b081173a5686e66dac4
Domenicopezzullo <pezzullodomenico712@gmail.com>
2025-02-08 19:31:43 +0100

    hi

--- index.ts
+++ index.ts
@@ -1,34 +1,33 @@
-import {dlopen, FFIType, suffix} from "bun:ffi"
+import { dlopen, FFIType, suffix } from "bun:ffi";
 const { i32, f32 } = FFIType;
 
-
-const path = `lib/libf.${suffix}`
+const path = `lib/libf.${suffix}`;
 
 const lib = dlopen(path, {
   cos: {
     args: [f32],
-    returns: f32
+    returns: f32,
   },
   add: {
     args: [i32, i32],
-    returns: i32
+    returns: i32,
   },
   subtract: {
     args: [i32, i32],
-    returns: i32
+    returns: i32,
   },
   multiply: {
     args: [i32, i32],
-    returns: i32
+    returns: i32,
   },
   divide: {
     args: [i32, i32],
-    returns: i32
-  }
-})
+    returns: i32,
+  },
+});
 
-console.log(lib.symbols.add(5, 10))
-console.log(lib.symbols.subtract(5, 10))
-console.log(lib.symbols.multiply(5, 10))
-console.log(lib.symbols.divide(10, 5))
-console.log(lib.symbols.cos(3))
+console.log(lib.symbols.add(5, 10));
+console.log(lib.symbols.subtract(5, 10));
+console.log(lib.symbols.multiply(5, 10));
+console.log(lib.symbols.divide(10, 5));
+console.log(lib.symbols.cos(3));
